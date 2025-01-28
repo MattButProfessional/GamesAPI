b4c26b7e05ecb8746497a2a4240edb700bdfd21c
MattButProfessional <bhammatt21@gmail.com>
2025-01-28 09:59:49 -0500

    100%

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger/swaggerConfig');
- 
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger/swaggerConfig");
+
 // Options for Swagger UI
 const swaggerOptions = {
   swaggerOptions: {
@@ -10,10 +10,10 @@ const swaggerOptions = {
     defaultModelExpandDepth: 2, // Sets depth for model expansion
   },
 };
- 
+
 const PORT = 3001;
 const app = express();
- 
+
 const gameRoutes = require("./routes/gamesRoutes");
 const platformRoutes = require("./routes/platformsRoutes");
 const screenshotRoutes = require("./routes/screenshotsRoutes");
@@ -23,27 +23,22 @@ const game_modeRoutes = require("./routes/game_modesRoutes");
 const websiteRoutes = require("./routes/websitesRoutes");
 const similarRoutes = require("./routes/similarsRoutes");
 const coverRoutes = require("./routes/coversRoutes");
- 
+
 app.use("/api/games", gameRoutes);
-app.use("/api/platforms", platformRoutes); 
-app.use("/api/screenshots", screenshotRoutes); 
-app.use("/api/characters", characterRoutes); 
-app.use("/api/genres", genreRoutes); 
-app.use("/api/game_modes", game_modeRoutes); 
-app.use("/api/websites", websiteRoutes); 
-app.use("/api/similar", similarRoutes); 
-app.use("/api/covers", coverRoutes); 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerOptions));
- 
-app.listen(PORT, ()=>{
-    console.log(`Try going to http://localhost:${PORT}/api/games`)
-    console.log(`Try going to http://localhost:${PORT}/api/platforms`)
-    console.log(`Try going to http://localhost:${PORT}/api/screenshots`)
-    console.log(`Try going to http://localhost:${PORT}/api/characters`)
-    console.log(`Try going to http://localhost:${PORT}/api/genres`)
-    console.log(`Try going to http://localhost:${PORT}/api/game_modes`)
-    console.log(`Try going to http://localhost:${PORT}/api/websites`)
-    console.log(`Try going to http://localhost:${PORT}/api/similar`)
-    console.log(`Try going to http://localhost:${PORT}/api/covers`)
-    console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`)
-})
+app.use("/api/platforms", platformRoutes);
+app.use("/api/screenshots", screenshotRoutes);
+app.use("/api/characters", characterRoutes);
+app.use("/api/genres", genreRoutes);
+app.use("/api/game_modes", game_modeRoutes);
+app.use("/api/websites", websiteRoutes);
+app.use("/api/similar", similarRoutes);
+app.use("/api/covers", coverRoutes);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, swaggerOptions)
+);
+
+app.listen(PORT, () => {
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+});
