556fa81e3ad81c35b8b41c4caff237c710256a79
MattButProfessional <bhammatt21@gmail.com>
2025-02-05 09:51:52 -0500

    jajajaja

--- controllers/views/gamePageController.js
+++ controllers/views/gamePageController.js
@@ -1,10 +1,28 @@
 const axios = require("axios");
 const settings = require("../../config/settings");
 async function renderGamePage(req, res) {
-  let data = { game_id: req.params.id };
+  const response = await axios.get(
+    `${settings.ROOT}:${settings.PORT}/api/games/${req.params.id}`
+  );
+  let data = response.data;
+  const cover = await getGameDataFromEndpoint(req.params.id, "covers");
+
+  data.cover = cover[0] ? cover[0].url : "";
+
+  const genres = await getGameDataFromEndpoint(req.params.id, "genres");
+  data.genres = genres;
+  console.log(data);
   res.render("game", {
-    title: "Games!",
+    title: "CMP - IGDB: " + data.name,
     gameData: data,
   });
+}
+
+async function getGameDataFromEndpoint(gameid, endpoint) {
+  let values = await axios.get(
+    `${settings.ROOT}:${settings.PORT}/api/${endpoint}`,
+    { params: { gameid: gameid } }
+  );
+  return values.data;
 }
 module.exports = { renderGamePage };
